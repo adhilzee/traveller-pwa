d4bfd4eec88483fcc8a1f24c3c72070b0fd6099e
adhilzee <153197478+adhilzee@users.noreply.github.com>
2026-03-13 00:56:06 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,102 +1,130 @@
+var askPrompt = null;
+var button = document.getElementById("install-button");
+
 $(document).ready(function () {
+
+  // LOAD CATEGORIES
   $.ajax({
     url: "https://traveller.talrop.works/api/v1/places/categories/",
     success: function (response) {
+
       let categories = response.data;
       let categories_html = "";
+
       categories.forEach((category) => {
-        categories_html += `<li>
-                <a href="#">
-                    <img
-                        class="rest"
-                        src="${category.image}"
-                        alt="${category.name}"
-                    />
-                    <span>${category.name}</span>
-                </a>
-            </li>`;
+        categories_html += `
+          <li>
+            <a href="#">
+              <img class="rest" src="${category.image}" alt="${category.name}" />
+              <span>${category.name}</span>
+            </a>
+          </li>`;
       });
 
       $("div.head ul").append(categories_html);
     },
   });
+
+  // LOAD PLACES
   $.ajax({
     url: "https://traveller.talrop.works/api/v1/places/",
     success: function (response) {
+
       let places = response.data;
       let places_html = "";
+
       places.forEach((place) => {
-        places_html += `<div class="item">
-                <a href="detail.html?id=${place.id}">
-                    <div class="top">
-                        <img src="${place.image}" alt="${place.name}" />
-                    </div>
-                    <div class="middle"><h3>${place.name}</h3></div>
-                    <div class="bottom">
-                        <img src="images/place.svg" alt="Image" />
-                        <span>${place.location}</span>
-                    </div>
-                </a>
-            </div>`;
+        places_html += `
+          <div class="item">
+            <a href="detail.html?id=${place.id}">
+              <div class="top">
+                <img src="${place.image}" alt="${place.name}" />
+              </div>
+              <div class="middle">
+                <h3>${place.name}</h3>
+              </div>
+              <div class="bottom">
+                <img src="images/place.svg" alt="Image" />
+                <span>${place.location}</span>
+              </div>
+            </a>
+          </div>`;
       });
 
       $("div.items").append(places_html);
     },
   });
-  var button = document.getElementById("install-button");
 
+  // INSTALL BUTTON LOGIC
   if (button) {
 
-  // hide install button if not on index page
-    if (!window.location.pathname.includes("index.html") && window.location.pathname !== "/traveller-pwa/") {
-      button.style.display = "none";
-    }
-
-  // hide if app already installed
+    // Hide button if running as installed app
     if (window.matchMedia('(display-mode: standalone)').matches) {
       button.style.display = "none";
     }
 
     button.addEventListener("click", installApp);
   }
- 
+
 });
 
-var askPrompt;
 
-// var button = document.getElementById("install-button");
-
+// SERVICE WORKER REGISTER
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("./sw.js").then(function () {
     console.log("Service worker registered!");
   });
 }
 
+
+// INSTALL FUNCTION
 const installApp = () => {
+
   if (askPrompt) {
+
     askPrompt.prompt();
+
     askPrompt.userChoice.then((result) => {
+
       console.log(result.outcome);
+
       if (result.outcome === "dismissed") {
         console.log("User dismissed");
       } else {
-        console.log("installed");
+        console.log("App installed");
       }
 
       askPrompt = null;
+
     });
   }
+
 };
 
-// button.addEventListener("click", installApp);
 
+// TRIGGER INSTALL PROMPT
 window.addEventListener("beforeinstallprompt", (event) => {
+
   console.log("before install prompt triggered");
-  askPrompt = event;
+
   event.preventDefault();
-  button.style.display = "block";
+
+  askPrompt = event;
+
+  if (button) {
+    button.style.display = "block";
+  }
+
 });
+
+
+// AFTER INSTALL HIDE BUTTON
 window.addEventListener("appinstalled", () => {
+
   console.log("App installed");
-  button.style.display = "none";
+
+  if (button) {
+    button.style.display = "none";
+  }
+
 });
